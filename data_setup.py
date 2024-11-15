7af227f7c7f8e21d46f3cd7c762aa5e384947a8b
Hugo <TUANkiet2404@gmail.com>
2024-11-15 20:36:12 +0700

    going modular

--- data_setup.py
+++ data_setup.py
@@ -1,4 +1,35 @@
+import torch
 import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+
 def read_data(current_path, data_type, track, language):
     data_path = current_path / 'data' / data_type / track / f'{language}.csv'
     return pd.read_csv(data_path)
+
+# @title Class to handle raw data
+class RawData(Dataset):
+    def __init__(self, data, labels, tokenizer, max_token):
+        self.data = data
+        self.labels = labels
+        self.max_token = max_token
+        self.tokenizer = tokenizer
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self, idx):
+        id = self.data['id'].iloc[idx]
+        text = self.data['text'].iloc[idx]
+
+        tokenizer_output = self.tokenizer(text, padding='max_length',
+                                        truncation=True, max_length=self.max_token, return_tensors='pt')
+
+        input_ids = tokenizer_output.input_ids[0]
+        attention_mask = tokenizer_output.attention_mask[0]
+
+        labels = torch.Tensor(self.data[self.labels].iloc[idx].values)
+
+        data = {'id': id,
+                'text': text,
+                'input_ids': input_ids,
+                'attention_mask': attention_mask,
+                'labels': labels}
+        return data
---
+++ utils.py
@@ -0,0 +1,96 @@
+import torch
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+# @title Get labels function
+def get_labels(data):
+    labels = data.columns[2:].tolist()
+    labels_to_ids = {label:id for id, label in enumerate(labels)}
+    ids_to_labels = {id:label for label, id in labels_to_ids.items()}
+    return labels, labels_to_ids, ids_to_labels
+
+# @title Train function
+def train_step(args, plm, cls, loss_function, optimizer, dataloader):
+    plm.train()
+    cls.train()
+    loss_total = 0
+    for batch_index, data in enumerate(dataloader):
+
+        input_ids = data['input_ids'].to(args.DEVICE)
+        attention_mask = data['attention_mask'].to(args.DEVICE)
+        labels = data['labels'].to(args.DEVICE)
+
+        plm_output = plm(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:, 0, :]
+        logit = cls(plm_output)
+
+        loss = loss_function(logit, labels)
+        optimizer.zero_grad()
+
+        loss.backward()
+        optimizer.step()
+
+        loss_total += loss
+        if (batch_index + 1) % 10 == 0:
+            print(f'Loss after {batch_index + 1} batches: {loss:.5f}')
+        torch.cuda.empty_cache()
+
+    loss_average = loss_total / len(dataloader)
+    return loss_total, loss_average
+
+# @title Test function
+def test_step(args, plm, cls, dataloader):
+    plm.eval()
+    cls.eval()
+    labels_true, labels_pred = [], []
+    with torch.inference_mode():
+        for batch_index, data in enumerate(dataloader):
+            input_ids = data['input_ids'].to(args.DEVICE)
+            attention_mask = data['attention_mask'].to(args.DEVICE)
+            labels = data['labels']
+
+            plm_output = plm(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:, 0, :]
+            logit = cls(plm_output)
+
+            torch.cuda.empty_cache()
+
+            batch_labels_pred = (torch.sigmoid(logit) >= 0.5).to(torch.int)
+
+            labels_true.extend(labels.to(torch.int).tolist())
+            labels_pred.extend(batch_labels_pred.tolist())
+    return labels_true, labels_pred
+
+# @title Metrics function
+def get_metrics(labels_true, labels_pred, labels_to_ids):
+    cls_report = metrics.classification_report(labels_true, labels_pred, target_names=labels_to_ids, zero_division=0.0, digits=5)
+    cls_report_dict = metrics.classification_report(labels_true, labels_pred, target_names=labels_to_ids, zero_division=0.0, output_dict=True)
+    f1_micro, f1_macro = cls_report_dict['micro avg']['f1-score'], cls_report_dict['macro avg']['f1-score']
+    return cls_report, f1_micro, f1_macro
+
+# @title Plot confusion matrix function
+def plot_confmat(labels_true, labels_pred, ids_to_labels):
+    confmat = metrics.multilabel_confusion_matrix(labels_true, labels_pred, labels=list(ids_to_labels.keys()))
+    fig, axes = plt.subplots(1, len(ids_to_labels), figsize=(25, 5))
+    for i in range(len(ids_to_labels)):
+        disp = metrics.ConfusionMatrixDisplay(confmat[i])
+        disp.plot(ax=axes[i], values_format='.4g')
+        disp.ax_.set_title(f'{list(ids_to_labels.values())[i]}')
+        if i != 0: disp.ax_.set_ylabel('')
+        if i != int(len(ids_to_labels) / 2): disp.ax_.set_xlabel('')
+        disp.im_.colorbar.remove()
+    no_of_labels = torch.tensor(labels_true).sum(dim = 0)
+    label_nums = {value:num.item() for value, num in zip(ids_to_labels.values(), no_of_labels)}
+    print(f'Number of true labels: {label_nums}')
+    plt.show()
+
+# @title Save models function
+def save_models(epoch, f1_micro, f1_macro, best_epoch, best_f1_micro, best_f1_macro, plm, cls, optimizer, MODEL_PATH):
+    if f1_micro > best_f1_micro and f1_macro > best_f1_macro:
+        best_f1_micro, best_f1_macro, best_epoch = f1_micro, f1_macro, epoch
+        MODEL_NAME = f"{round(best_f1_micro, 4)}_{round(best_f1_macro, 4)}.pt"
+        MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+        print(f'** Best model found in this epoch ({epoch}), saving model to: {MODEL_SAVE_PATH} **')
+        state = {"plm": plm.state_dict(),
+                "cls": cls.state_dict(),
+                "optimizer": optimizer.state_dict()}
+        torch.save(state, MODEL_SAVE_PATH)
+    return best_epoch, best_f1_micro, best_f1_macro
